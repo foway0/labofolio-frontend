cb0de50e8b4d2f567fb85f38127c228069a638aa
foway <foway0@gmail.com>
2019-07-18 02:51:20 +0900

    add build watch & uglify & svelte fetch test

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,14 @@
+import path from 'path'
+
 import pkg from './package.json';
 import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import serve from 'rollup-plugin-serve';
+import { terser } from 'rollup-plugin-terser';
+
+const mode = process.env.SERVICE_ENV || 'local';
 
 export default {
   input: 'src/index.js',
@@ -20,5 +27,17 @@ export default {
     }),
     resolve(),
     commonjs(),
-  ]
+    mode === 'local' && livereload('public'),
+    // FIXME prd
+    mode === 'local' && terser(),
+    serve({
+      contentBase: path.join(__dirname, 'dist'),
+      historyApiFallback: true,
+      host: 'localhost',
+      port: 8080,
+    })
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
--- src/test.svelte
+++ src/test.svelte
@@ -1,11 +1,32 @@
-<script >
-    export let name;
+<script>
+  export let name;
+  let promise = getSample();
+
+  async function getSample() {
+    const res = await fetch(`http://localhost/sample/1`);
+    const text = await res.text();
+    console.log(text);
+
+    if (res.ok) {
+      return JSON.stringify(text);
+    } else {
+      throw new Error(text);
+    }
+  }
 </script>
 
 <style>
-    h1 {
-        color: blue;
-    }
+  h1 {
+    color: blue;
+  }
 </style>
 
 <h1>{name}!!!</h1>
+
+{#await promise}
+  <p>...waiting</p>
+{:then stringify}
+  <p>The result is {stringify}</p>
+{:catch error}
+  <p style="color: red">{error.message}</p>
+{/await}
