6747da617170588c09bafd4e81f8d24e0475cf1f
GARAMKIM <kim_g@camobile.com>
2020-09-15 20:38:52 +0900

    pascal case → snake case

--- src/Main.svelte
+++ src/Main.svelte
@@ -19,10 +19,10 @@ let promise = list();
   {#await promise}
     <p>...waiting</p>
   {:then object}
-    <p>The count is {object.Count}</p>
-    {#each object.Rows as item}
-      <p>Id: {item.Id} {item.Subject}</p>
-      <p>{item.ContentMd}</p>
+    <p>The count is {object.count}</p>
+    {#each object.rows as item}
+      <p>Id: {item.id} {item.subject}</p>
+      <p>{item.content_md}</p>
       <hr>
     {/each}
   {:catch error}
